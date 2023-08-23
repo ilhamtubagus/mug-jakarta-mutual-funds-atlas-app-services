9dcbedd8ed0125966099b1e822afa953cf83bc27
Ilham.Arfian <ilham.arfian@btpn.com>
2023-08-23 15:47:16 +0700

    add functions find product by product code

---
+++ functions/findProductByCode.js
@@ -0,0 +1,78 @@
+const pipeline = (productCode) => [
+  {
+    $match: {
+      productCode: {
+        $eq: productCode,
+      },
+    },
+  },
+  {
+    $lookup: {
+      from: 'investmentManagers',
+      localField: 'investmentManager',
+      foreignField: 'investmentManagerCode',
+      as: 'investmentManager',
+    },
+  },
+  {
+    $lookup: {
+      from: 'navs',
+      let: {
+        productCode: '$productCode',
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: [
+                '$productCode',
+                '$$productCode',
+              ],
+            },
+          },
+        },
+        {
+          $sort: {
+            createdAt: -1,
+          },
+        },
+        {
+          $limit: 1,
+        },
+      ],
+      as: 'nav',
+    },
+  },
+  {
+    $addFields: {
+      nav: {
+        $first: '$nav',
+      },
+      investmentManager: {
+        $first: '$investmentManager',
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      'investmentManager._id': 0,
+      'nav._id': 0,
+      'nav.productCode': 0,
+    },
+  },
+];
+const findProductByCode = async (productCode) => {
+  const databaseName = context.environment.values.databaseName || 'mutual-funds';
+  const mongodb = context.services.get('mongodb-atlas');
+  const productsCollection = mongodb.db(databaseName).collection('products');
+
+  return productsCollection.aggregate(pipeline(productCode));
+};
+
+exports = findProductByCode;
+
+/* istanbul ignore next */
+if (typeof module !== 'undefined') {
+  module.exports = findProductByCode;
+}
---
+++ tests/unit/findProductByCode.spec.js
@@ -0,0 +1,108 @@
+const findProductByCode = require('../../functions/findProductByCode');
+
+describe('#findProductByCode', () => {
+  const productCode = 'SCHPU';
+
+  beforeEach(() => {
+    const aggregate = jest.fn();
+    const collection = jest.fn().mockReturnValue({ aggregate });
+    const db = jest.fn().mockReturnValue({ collection });
+    const get = jest.fn().mockReturnValue({ db });
+
+    collection.aggregate = aggregate;
+    db.collection = collection;
+    get.db = db;
+
+    global.context = {
+      environment: {
+        values: {
+          databaseName: 'mutual-funds',
+        },
+      },
+      services: {
+        get,
+      },
+    };
+  });
+
+  it('should invoke aggregation pipeline on products collection', async () => {
+    const expectedPipeline = [
+      {
+        $match: {
+          productCode: {
+            $eq: productCode,
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'investmentManagers',
+          localField: 'investmentManager',
+          foreignField: 'investmentManagerCode',
+          as: 'investmentManager',
+        },
+      },
+      {
+        $lookup: {
+          from: 'navs',
+          let: {
+            productCode: '$productCode',
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: [
+                    '$productCode',
+                    '$$productCode',
+                  ],
+                },
+              },
+            },
+            {
+              $sort: {
+                createdAt: -1,
+              },
+            },
+            {
+              $limit: 1,
+            },
+          ],
+          as: 'nav',
+        },
+      },
+      {
+        $addFields: {
+          nav: {
+            $first: '$nav',
+          },
+          investmentManager: {
+            $first: '$investmentManager',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          'investmentManager._id': 0,
+          'nav._id': 0,
+          'nav.productCode': 0,
+        },
+      },
+    ];
+
+    await findProductByCode(productCode);
+
+    expect(context.services.get.db).toBeCalledWith(context.environment.values.databaseName);
+    expect(context.services.get.db.collection).toBeCalledWith('products');
+    expect(context.services.get.db.collection.aggregate).toBeCalledWith(expectedPipeline);
+  });
+
+  it('should set db name with default database name if value in environment not set', async () => {
+    delete global.context.environment.values.databaseName;
+
+    await findProductByCode(productCode);
+
+    expect(context.services.get.db).toBeCalledWith('mutual-funds');
+  });
+});
